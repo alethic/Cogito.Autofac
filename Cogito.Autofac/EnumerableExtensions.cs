b66d35fe7a27abd4e30b2d0c28bcaefeb102ebc4
Jerome Haltom <wasabi@cogito.cx>
2019-05-22 01:01:14 -0500

    Added some missing stuff from FNS.

---
+++ Cogito.Autofac/EnumerableExtensions.cs
@@ -0,0 +1,32 @@
+﻿#if NET462 || NET47
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cogito.Autofac
+{
+
+    static class EnumerableExtensions
+    {
+
+        /// <summary>
+        /// Prepends the given item to the front of the enumerable.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        static IEnumerable<T> Prepend<T>(this IEnumerable<T> source, T item)
+        {
+            yield return item;
+
+            foreach (var i in source)
+                yield return i;
+        }
+
+    }
+
+}
+
+#endif
