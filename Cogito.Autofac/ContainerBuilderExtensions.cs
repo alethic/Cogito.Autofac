93acb90229f374f9f3708fbf301ac4d3a1d6ada3
Jerome Haltom <jhaltom@alethic.solutions>
2021-10-18 12:19:51 -0500

    Add overloads to filter out modules by base type.

--- Cogito.Autofac/ContainerBuilderExtensions.cs
+++ Cogito.Autofac/ContainerBuilderExtensions.cs
@@ -3,6 +3,7 @@ using System.Linq;
 
 using Autofac;
 using Autofac.Builder;
+using Autofac.Core;
 using Autofac.Core.Registration;
 
 using Cogito.Reflection;
@@ -33,6 +34,23 @@ namespace Cogito.Autofac
             return builder.RegisterAssemblyModules(SafeAssemblyLoader.LoadAll(dependencyContext).ToArray());
         }
 
+        /// <summary>
+        /// Registers all modules found in all assemblies.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="dependencyContext"></param>
+        /// <returns></returns>
+        public static IModuleRegistrar RegisterAllAssemblyModules<TModule>(this ContainerBuilder builder, DependencyContext dependencyContext)
+            where TModule : IModule
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+            if (dependencyContext is null)
+                throw new ArgumentNullException(nameof(dependencyContext));
+
+            return builder.RegisterAssemblyModules<TModule>(SafeAssemblyLoader.LoadAll(dependencyContext).ToArray());
+        }
+
 #endif
 
         /// <summary>
@@ -46,6 +64,20 @@ namespace Cogito.Autofac
                 throw new ArgumentNullException(nameof(builder));
 
             return builder.RegisterAssemblyModules(SafeAssemblyLoader.LoadAll().ToArray());
+        }
+
+        /// <summary>
+        /// Registers all modules found in all assemblies.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IModuleRegistrar RegisterAllAssemblyModules<TModule>(this ContainerBuilder builder)
+            where TModule : IModule
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            return builder.RegisterAssemblyModules<TModule>(SafeAssemblyLoader.LoadAll().ToArray());
         }
 
         /// <summary>
