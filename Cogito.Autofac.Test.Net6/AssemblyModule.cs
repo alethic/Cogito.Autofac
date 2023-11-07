0a93d67db6db54c166fd77257503745aab8bb412
Jerome Haltom <jhaltom@alethic.solutions>
2023-11-07 12:18:07 -0600

    Update to latest versions of dependencies. Change appveyor key.

---
+++ Cogito.Autofac.Test.Net6/AssemblyModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+
+namespace Cogito.Autofac.Test.Net6
+{
+
+    public class AssemblyModule : ModuleBase
+    {
+
+        protected override void Register(ContainerBuilder builder)
+        {
+            builder.RegisterFromAttributes(typeof(AssemblyModule).Assembly);
+        }
+
+    }
+
+}
